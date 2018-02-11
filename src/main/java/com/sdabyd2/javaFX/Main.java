16c2adc500fd6232683cf1b18161aac149b714b4
WaldemarCholewinski <cholewinskii@gmail.com>
2018-02-11 15:50:24 +0100

    code update

--- src/main/java/com/sdabyd2/javaFX/Main.java
+++ src/main/java/com/sdabyd2/javaFX/Main.java
@@ -1,6 +1,9 @@
 package com.sdabyd2.javaFX;
 
+import com.sdabyd2.javaFX.model.Person;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,6 +17,22 @@ public class Main extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+
+    private ObservableList<Person> personObsevableList = FXCollections.observableArrayList();
+
+    public Main(){
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+        personObsevableList.add(new Person("Jan","Kowalski"));
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
--- src/main/java/com/sdabyd2/javaFX/controller/Controller.java
+++ src/main/java/com/sdabyd2/javaFX/controller/Controller.java
@@ -1,4 +1,46 @@
 package com.sdabyd2.javaFX.controller;
 
+import com.sdabyd2.javaFX.model.Person;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import javax.swing.text.TabableView;
+
 public class Controller {
+
+    @FXML
+    private TableView<Person> personTableView;
+
+    @FXML
+    private TableColumn<Person, String> firstname;
+
+    @FXML
+    private TableColumn<Person, String> lastname;
+
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private Label lastnameLabel;
+
+    @FXML
+    private Label streetLabel;
+
+    @FXML
+    private Label postalCodeLabel;
+
+    @FXML
+    private Label cityLabel;
+
+    @FXML
+    private Label birthdayLabel;
+
+
+
+
+
+
+
 }
